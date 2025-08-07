1b7c61cedf16f4d07fe3df5e72b38f442f938630
divinenwokedi667 <divinenwokedi@gmail.com>
2025-08-06 23:11:18 -0400

    Added scraper folder to repo

---
+++ dblegends-scraper/scrape.js
@@ -0,0 +1,65 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+let count = 0;
+const total = 697+2;
+
+
+const characters = [];
+
+const BASE_URL = 'https://dblegends.net/character/';
+
+async function scrapeCharacter(id) {
+    try {
+        const response = await axios.get(`${BASE_URL}${id}`);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const cardNum = $('#overlaybg div.cardNUM').text().trim();
+
+        const name = $('#overlaybg > div.name, #overlaybg > #SPShallotBaseImage div.name').text().trim();
+
+        const image = 'https://dblegends.net/' + $('#overlaybg img.cutin').attr('src');
+
+        const rarity = $('#overlaybg div.rarity').attr('class').split(' ')[1];
+
+        const color = $('#overlaybg div.element').attr('class').split(' ')[1];
+
+        const tags = new Set();
+        $('div.trait-container a.trait-thumb .name').each((i, el) => {
+            tags.add($(el).text().trim());
+        });
+
+        const characterData = {
+            id,
+            cardNum,
+            name,
+            image,
+            rarity,
+            color,
+            tags: [...tags]
+        };
+
+        characters.push(characterData);
+        count++;
+        process.stdout.write(`Characters saved: ${count}/${total}\r`);
+
+    } catch(err) {
+        if (err.response && err.response.status === 404) {
+            console.log(`ID: ${id} not found.`);
+        } else {
+            console.error(`Error with ID ${id}:`, err.message);
+        }
+    }
+}
+
+(async () => {
+    for (let id = 1; id <= total-2; id++) {
+        await scrapeCharacter(id);
+    }
+    await scrapeCharacter(19800);
+    await scrapeCharacter(19000);
+
+    fs.writeFileSync('characters.json', JSON.stringify(characters, null, 2));
+    console.log('✅ Character data saved successfully!');
+})();
